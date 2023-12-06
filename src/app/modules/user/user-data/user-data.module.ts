7d83bec5bf0959b8e5b90cd4faa10e5623e05772
viren-rathod <viren.rathod.2023@gmail.com>
2023-12-06 16:15:58 +0530

    Working on user module

---
+++ src/app/modules/user/user-data/user-data.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserDataRoutingModule } from './user-data-routing.module';
+import { UserDataComponent } from './user-data.component';
+
+
+@NgModule({
+  declarations: [
+    UserDataComponent
+  ],
+  imports: [
+    CommonModule,
+    UserDataRoutingModule
+  ]
+})
+export class UserDataModule { }
