e02e7b760754f816e154c830712c14584748c8d9
viren.rathod <viren.rathod@esparkbiztech.com>
2025-05-18 11:30:06 +0530

    Added Google Login support

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { NgxPermissionsModule } from 'ngx-permissions';
 import { ModalComponent } from './components/modal/modal.component';
 import { LoaderComponent } from './components/loader/loader.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [ModalComponent, LoaderComponent],
-  imports: [CommonModule, NgxPermissionsModule.forRoot()],
+  declarations: [ModalComponent, LoaderComponent, NotFoundComponent],
+  imports: [CommonModule, NgxPermissionsModule.forRoot(),RouterModule],
   exports: [ModalComponent, LoaderComponent, CommonModule],
 })
 export class SharedModule {}
